976e508e57db4027675bd6a315dcf313b30d98ae
EnverSiraz <130348232+EnverSiraz@users.noreply.github.com>
2023-06-26 03:25:46 +0300

    functions done

--- src/Components/WeatherReport.jsx
+++ src/Components/WeatherReport.jsx
@@ -5,76 +5,54 @@ import { useEffect } from 'react'
 
 
 function WeatherReport() {
-    // navigator.geolocation.getCurrentPosition(pos => { const { latitude, longitude } = pos.coords; console.log(latitude, longitude) })
-    // `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
-   
-   
 
-    const [address, setaddress] = useState([])
+    const [address, setaddress] = useState("")
+    const [city, setcity] = useState("")
+
 
 
     useEffect(() => {
 
-
-
     }, [])
-
-
-
-
-
-
-
-
-    // const GetCoordinates = () => {
-    //     navigator.geolocation.getCurrentPosition(pos => { const { latitude, longitude } = pos.coords; setlat(latitude); setlong(longitude); console.log({ lat }, { long }) })
-    // }
 
 
     const [coords, setcoords] = useState("")
 
     const GetCoordinates = () => {
-        navigator.geolocation.getCurrentPosition(pos => { setcoords(pos.coords);console.log(coords.latitude,coords.longitude)})
-        
+        navigator.geolocation.getCurrentPosition(pos => { setcoords(pos.coords) })
+        console.log(coords.latitude,coords.longitude)
+
     }
-       
-     
-           
 
 
 
+    const GetData = () => {
+        axios.get(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${coords.latitude}&lon=${coords.longitude}`)
+            .then(res => setaddress(res.data));
+
+        setcity(address.address.province)
+        console.log(address.address.province);
+        console.log(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${coords.latitude}&lon=${coords.longitude}`)
+    }
 
 
 
-
-// const loaddata = () => {
-//     axios.get("https://api.weatherbit.io/v2.0/current?city=Istanbul&country=TR&key=672f913578e4404e99164b83dc0e4854")
-//         .then(res => setcity(res.data.data[0].city_name))
-
-
-
-// }
+    const loaddata = () => {
+        axios.get(`https://api.weatherbit.io/v2.0/current?city=${city}&country=TR&key=672f913578e4404e99164b83dc0e4854`)
+            .then(res => console.log(res.data.data[0].temp))
+    }
 
 
+    return (
+        <>
 
+            <h1>asdsadaasdasd</h1>
+            <button onClick={() => loaddata()}>BUTON1</button>
+            <button onClick={() => GetData()}>BUTON2</button>
+            <button onClick={() => GetCoordinates()}>BUTON3</button>
 
-return (
-    <>
-
-        <h1>asdsadaasdasd</h1>
-        <button onClick={() => GetCoordinates()}>BUTON1</button>
-
-
-        <p>road : {address.road}</p>
-        <p>city : {address.province}</p>
-        <p>country :{address.country}</p>
-
-
-
-
-
-    </>
-)
+        </>
+    )
 }
 
 export default WeatherReport
